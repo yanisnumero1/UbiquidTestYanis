a822abed1e99f1ca9128c96cef253a465715c582
Yanis Salatnia <yanis.salatnia@gmail.com>
2021-06-30 21:31:14 +0200

    impelmentation nav bar

---
+++ app/src/main/java/fr/yanisnumero1/ubiquidtestyanis/HomeActivity.java
@@ -0,0 +1,14 @@
+package fr.yanisnumero1.ubiquidtestyanis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/fr/yanisnumero1/ubiquidtestyanis/MainActivity.java
+++ app/src/main/java/fr/yanisnumero1/ubiquidtestyanis/MainActivity.java
@@ -6,15 +6,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,10 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
     private CodeScannerView mCodeScannerView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},123 );
@@ -34,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+
 
     private void startScanning() {
 
