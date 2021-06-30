5445036696cad0abfb3a299a217ebe9f49d12d39
Yanis Salatnia <yanis.salatnia@gmail.com>
2021-06-30 18:36:19 +0200

    ajout layout et logique

--- app/src/main/java/fr/yanisnumero1/ubiquidtestyanis/MainActivity.java
+++ app/src/main/java/fr/yanisnumero1/ubiquidtestyanis/MainActivity.java
@@ -1,14 +1,75 @@
 package fr.yanisnumero1.ubiquidtestyanis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    private CodeScannerView mCodeScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},123 );
+        } else{
+            startScanning();
+
+        }
+    }
+
+    private void startScanning() {
+
+        mCodeScannerView=findViewById(R.id.scanner_view);
+        mCodeScanner= new CodeScanner(this, mCodeScannerView);
+        mCodeScanner.startPreview();
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        mCodeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCodeScanner.startPreview();
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,@NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission Autorisée", Toast.LENGTH_SHORT).show();
+                startScanning();
+            }else{
+                Toast.makeText(this, "Permission refusée", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
